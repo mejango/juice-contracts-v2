cf53cc686c4773e6f5d2258e64dbed8a860fff61
Jorge Garcia <jagbolanos@gmail.com>
2022-03-04 20:46:36 -0800

    Validation added, now test breaks, need to set the trusted caller

--- contracts/StudioDAOPayDelegate.sol
+++ contracts/StudioDAOPayDelegate.sol
@@ -5,8 +5,10 @@ import './interfaces/IJBPayDelegate.sol';
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NFTRewards is ERC721URIStorage, IJBPayDelegate  {
+contract NFTRewards is ERC721URIStorage, IJBPayDelegate, Ownable, AccessControl  {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -14,9 +16,21 @@ contract NFTRewards is ERC721URIStorage, IJBPayDelegate  {
     uint256 constant SILVER  = 10000000000000000;
     uint256 constant GOLD    = 1000000000000000000;
 
-    constructor() ERC721("StudioRewards", "STDIOR") {}
+    address trusted_caller;
+
+    bytes32 public constant TRUSTED_CALLER_ROLE = keccak256("TRUSTED_CALLER_ROLE");
+
+    constructor() ERC721("StudioRewards", "STDIOR") {
+        _setupRole(TRUSTED_CALLER_ROLE, msg.sender);
+    }
+
+    function setTrustedCaller(address new_trusted_caller) public onlyOwner {
+        _setupRole(TRUSTED_CALLER_ROLE, new_trusted_caller);
+    }
 
     function didPay(JBDidPayData calldata _param) public override {
+        require(hasRole(TRUSTED_CALLER_ROLE, msg.sender), "Caller is not a trusted caller");
+
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -31,6 +45,10 @@ contract NFTRewards is ERC721URIStorage, IJBPayDelegate  {
             _setTokenURI(newItemId, "https://gateway.pinata.cloud/ipfs/QmdkHedPr3et5kKQELy56M1fQvLBvPF9KkuTK9dTtpFRjg");
         }
         
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
     
 }
