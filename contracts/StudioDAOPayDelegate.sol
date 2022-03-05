4c61847ca81278f74e16a653d51386a3e8041603
Jorge Garcia <jagbolanos@gmail.com>
2022-03-04 18:18:42 -0800

    Tiered non-secure StudioDAOPayDelegate

--- contracts/StudioDAOPayDelegate.sol
+++ contracts/StudioDAOPayDelegate.sol
@@ -1,33 +1,36 @@
 pragma solidity ^0.8.0;
 
-//import "@manifoldxyz/creator-core-solidity/contracts/ERC721Creator.sol";
+//import "@jbx-protocol/contracts-v2/contracts/interfaces/IJBPayDelegate.sol";
 import './interfaces/IJBPayDelegate.sol';
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract NFTRewards is ERC721URIStorage, IJBPayDelegate  {
 
-// /!\ This is insecure in this way (no auth check in didPay -> everyone can mint)
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 constant BRONZE  = 1000000000000000;
+    uint256 constant SILVER  = 10000000000000000;
+    uint256 constant GOLD    = 1000000000000000000;
 
+    constructor() ERC721("StudioRewards", "STDIOR") {}
 
-//contract NFTRewards is ERC721Creator, IJBPayDelegate  {
-contract NFTRewards is ERC721URIStorage, IJBPayDelegate {
-  //constructor() ERC721Creator ("NFTRewards", "STDIO") {
-  //}
+    function didPay(JBDidPayData calldata _param) public override {
+        _tokenIds.increment();
 
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-
-  constructor() ERC721('NFTRewards', 'STDIO') {}
-
-  function didPay(JBDidPayData calldata _param) public override {
-    _tokenIds.increment();
-
-    uint256 newItemId = _tokenIds.current();
-    _mint(_param.payer, newItemId);
-    _setTokenURI(
-      newItemId,
-      'https://gateway.pinata.cloud/ipfs/QmXMLNsz7LNHA2JViLuUiCoGGVmnhnE3Vc76f2a5EtoGE6'
-    );
-  }
+        uint256 newItemId = _tokenIds.current();
+        _mint(_param.payer, newItemId);
+        if (_param.amount < BRONZE) {
+            _setTokenURI(newItemId, "https://gateway.pinata.cloud/ipfs/QmV4eDCDfYoaBcCoTU6ce9DSq2SFv4dEm7c8wkBEdWYVVD");
+        } else if (_param.amount >= BRONZE && _param.amount < SILVER) {
+            _setTokenURI(newItemId, "https://gateway.pinata.cloud/ipfs/QmY9T2yY65aCrASiwRuH98NxyQTbceSWuM5A1Mmg3S9CHA");
+        } else if (_param.amount >= SILVER && _param.amount < GOLD) {
+            _setTokenURI(newItemId, "https://gateway.pinata.cloud/ipfs/QmXupnWuoCF18ZX4CJcHZTVc5GjkYWorXqjypnEGSpaM3d");
+        } else if (_param.amount >= GOLD) {
+            _setTokenURI(newItemId, "https://gateway.pinata.cloud/ipfs/QmdkHedPr3et5kKQELy56M1fQvLBvPF9KkuTK9dTtpFRjg");
+        }
+        
+    }
+    
 }
